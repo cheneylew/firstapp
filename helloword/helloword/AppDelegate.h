dece494a3a38b485d3d08a17db8fa85c6d6c83f9
cheneylew <cheneylew@163.com>
2014-01-16 20:50:04 +0800

    build project

---
+++ helloword/helloword/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloword
+//
+//  Created by cheneylew on 1/16/14.
+//  Copyright (c) 2014 cheneylew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ helloword/helloword/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  helloword
+//
+//  Created by cheneylew on 1/16/14.
+//  Copyright (c) 2014 cheneylew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
